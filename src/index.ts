a40c4fa627a50f8fa8c62ccc63712d208a3f2e2e
Hafid Mosquera <136523416+srhafid@users.noreply.github.com>
2025-01-18 21:10:27 -0500

    traverser directement les composants

--- src/index.ts
+++ src/index.ts
@@ -69,54 +69,30 @@ export const grapeJsTailwindSupport = (editor: any, opts: Options = {}): void =>
 
 /**
  * Function to integrate custom UI components into GrapesJS with Tailwind UI integration.
- * It processes components in batches and adds them to the editor's block manager.
  * 
  * @param {any} editor - The GrapesJS editor instance.
- * @param {Array<Array<structureComponentGenerate>>} options - An array of categories, where each category contains a list of structureComponentGenerate objects.
- * @param {number} [batchSize=10] - The size of each batch to process at once.
- * @returns {Promise<void>} A promise that resolves once all components are processed and added to the block manager.
+ * @param {Array<ArraystructureComponentGenerate>} options
  */
-export const grapeJsTailwindUiComponents = async (
+export const grapeJsTailwindUiComponents = (
     editor: any,
-    options: Array<Array<structureComponentGenerate>>,
-    batchSize: number = 10
-): Promise<void> => {
+    components: Array<structureComponentGenerate>
+): void => {
     const blockManager = editor.Blocks;
 
-    /**
-     * Processes a single component and prepares it for addition to the block manager.
-     * 
-     * @param {any} source - The source component to process.
-     * @returns {object} The processed component ready to be added to the block manager.
-     */
-    const processComponent = (source: any) => {
-        const svgElement = editor.$(source.label).get(0) as SVGElement;
+    const processComponent = (component: structureComponentGenerate) => {
+        const svgElement = editor.$(component.label).get(0) as SVGElement;
         const svgHtml = getSvgHtml(svgElement);
         return {
-            id: source.id,
+            id: component.id,
             label: svgHtml,
-            attributes: { class: `${source.class} block-full-width` },
-            content: source.content,
-            category: { label: source.category, open: false },
+            attributes: { class: `${component.class} block-full-width` },
+            content: component.content,
+            category: { label: component.category, open: false },
         };
     };
 
-    /**
-     * Processes a batch of components and adds them to the block manager.
-     * 
-     * @param {Array<structureComponentGenerate[]>} batch - The batch of components to process.
-     * @returns {Promise<void>} A promise that resolves once the batch has been processed and the components are added to the block manager.
-     */
-    const processBatch = async (batch: Array<structureComponentGenerate[]>) => {
-        const components = batch
-            .map(category => category.map(processComponent))
-            .reduce((acc, curr) => acc.concat(curr), []);
-        components.forEach(component => blockManager.add(component.id, component));
-    };
-
-    // Processes options in batches
-    for (let i = 0; i < options.length; i += batchSize) {
-        const batch = options.slice(i, i + batchSize);
-        await processBatch(batch);
-    }
+    components.forEach(component => {
+        const processedComponent = processComponent(component);
+        blockManager.add(processedComponent.id, processedComponent);
+    });
 };
